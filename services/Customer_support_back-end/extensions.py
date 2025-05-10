cb23fe6bdec41f362b999f3a3bf75a10cd471ebd
SalmaHagi <saluhagi@gmail.com>
2025-05-10 14:36:17 +0300

    duplicate files

--- services/Customer_support_back-end/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_mail import Mail
-from flask_socketio import SocketIO
-
-db = SQLAlchemy()
-jwt = JWTManager()
-mail = Mail()
-socketio = SocketIO(cors_allowed_origins="*")
--- services/Orderservice/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-import os
-
-app = create_app()
-
-if __name__ == '__main__':
-    debug_mode = os.getenv('DEBUG_MODE', 'False').lower() == 'true'
-    app.run(debug=debug_mode, port=5005)
